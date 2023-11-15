39a3688a7b858dd1e3d076a1d80ab7a7064a9f84
Semyon <semhowork@gmail.com>
2023-11-15 09:23:49 +0300

    fix

--- client/components/auth/SignButton.tsx
+++ client/components/auth/SignButton.tsx
@@ -26,35 +26,6 @@ export default function SignButton({ isAuth = false }: Props) {
     }
   }, [session])
 
-  // return (
-  //   <>
-  //     {!isAuthState && (
-  //       <Link
-  //         href="/api/auth/signin"
-  //         className={`p-2 flex rounded-md hover:bg-slate-200 text-slate-700 hover:text-slate-700 relative transition duration-200 ease-in-out items-center ${
-  //           isActive ? "bg-slate-200" : ""
-  //         }`}
-  //       >
-  //         <UserIcon className="w-4 h-4" strokeWidth={2} />
-  //         <span>Авторизация</span>
-  //       </Link>
-  //     )}
-  //     {isAuthState && (
-  //       <Link
-  //         href="#"
-  //         className={`p-2 flex rounded-md hover:bg-slate-200 text-slate-700 hover:text-slate-700 relative transition duration-200 ease-in-out items-center ${
-  //           isActive ? "bg-slate-200" : ""
-  //         }`}
-  //         onClick={() => signOut({ callbackUrl: "/" })}
-  //       >
-  //         <span className="mr-2">{username}</span>
-  //         <ArrowRightOnRectangleIcon className="w-4 h-4" strokeWidth={2} />
-  //         <span>Выход</span>
-  //       </Link>
-  //     )}
-  //   </>
-  // )
-
   return (
     <>
       {!isAuthState && (
